a03ef98d77950459d88a66ac480eddbd2ae60c58
tehillasebrow <tsebrow@student.touro.edu>
2026-01-18 02:33:03 -0500

    fixing the seating planner to accomadate other guest types.

--- src/main/java/edu/course/eventplanner/service/SeatingPlanner.java
+++ src/main/java/edu/course/eventplanner/service/SeatingPlanner.java
@@ -12,86 +12,33 @@ public class SeatingPlanner {
     }
 
     public Map<Integer, List<Guest>> generateSeating(List<Guest> guests) {
+        Map<String, Queue<Guest>> groupQueues = new LinkedHashMap<>();
 
-        Queue<Guest> famQueue = new LinkedList<>();
-        Queue<Guest> frndQueue = new LinkedList<>();
-        Queue<Guest> nbrQueue = new LinkedList<>();
-        Queue<Guest> cwrkrQueue = new LinkedList<>();
+        for (Guest guest : guests) {
+            groupQueues.computeIfAbsent(guest.getGroupTag(), k -> new LinkedList<>()).add(guest);
+        }
+
 
 //goes through the guest list and adds them to the right queue according to their tag
-        for (Guest guest : guests) {
-            if (Objects.equals(guest.getGroupTag(), "family")) {
-                famQueue.add(guest);
-            }
-            if (Objects.equals(guest.getGroupTag(), "friends"))
-                frndQueue.add(guest);
-            if (Objects.equals(guest.getGroupTag(), "neighbors"))
-                nbrQueue.add(guest);
-            if (Objects.equals(guest.getGroupTag(), "coworkers"))
-                cwrkrQueue.add(guest);
-        }
+
         int tablesctr = venue.getTables();
         int seatsPerTablectr = venue.getSeatsPerTable();
         int capacity = venue.getCapacity();
 
 
-
         Map<Integer, List<Guest>> map = new TreeMap<>();
 //goes through the family list and adds them to the tables first. Table 10 fills up first, then table 9 ect...
-
-        while (!famQueue.isEmpty()) {
-            map.computeIfAbsent(tablesctr, k -> new ArrayList<>()).add(famQueue.remove());
-            seatsPerTablectr--;
-            if (seatsPerTablectr == 0 && tablesctr > 0) {//when the seats get to 0, we decrease the table number and add seats back to the seats per table ctr
-                tablesctr--;
-                seatsPerTablectr = venue.getSeatsPerTable();
+        for (Queue<Guest> queue : groupQueues.values()) {
+            while (!groupQueues.isEmpty()) {
+                map.computeIfAbsent(tablesctr, k -> new ArrayList<>()).add(queue.remove());
+                seatsPerTablectr--;
+                if (seatsPerTablectr == 0 && tablesctr > 0) {
+                    tablesctr--;
+                    seatsPerTablectr = venue.getSeatsPerTable();
+                }
             }
-            if (tablesctr == 0) {//if the table number is 0, we return the map because we have filled all the tables
 
-                return map;
-            }
         }
-
-        while (!frndQueue.isEmpty()) {
-            map.computeIfAbsent(tablesctr, k -> new ArrayList<>()).add(frndQueue.remove());
-            seatsPerTablectr--;
-            if (seatsPerTablectr == 0 && tablesctr >0) {
-                tablesctr--;
-                seatsPerTablectr = venue.getSeatsPerTable();
-
-            }
-            if (tablesctr == 0) {
-
-                return map;
-            }
-        }
-        while (!nbrQueue.isEmpty()) {
-            map.computeIfAbsent(tablesctr, k -> new ArrayList<>()).add(nbrQueue.remove());
-            seatsPerTablectr--;
-            if (seatsPerTablectr == 0 && tablesctr > 0) {
-                tablesctr--;
-                seatsPerTablectr = venue.getSeatsPerTable();
-
-            }
-            if (tablesctr == 0) {
-
-                return map;
-            }
-        }
-        while (!cwrkrQueue.isEmpty()) {
-            map.computeIfAbsent(tablesctr, k -> new ArrayList<>()).add(cwrkrQueue.remove());
-            seatsPerTablectr--;
-            if (seatsPerTablectr == 0 && tablesctr > 0) {
-                tablesctr--;
-                seatsPerTablectr = venue.getSeatsPerTable();
-
-            }
-            if (tablesctr == 0) {
-
-                return map;
-            }
-        }
-
 
         return map;
     }
